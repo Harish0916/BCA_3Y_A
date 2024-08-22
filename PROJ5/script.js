9e713bd56ea80717db0c67dc6c19b9ddd04d238b
harish0916 <harish0916ojhacs@gmail.com>
2024-08-22 14:53:58 +0530

    Object topic in Javascript

--- PROJ5/script.js
+++ PROJ5/script.js
@@ -23,21 +23,86 @@
 
 //--------------------
 
-var students = {
-    id: 101,        // number
-    sname: "Kapil", // string
-    isPassed: true, // boolean
-    img:"https://cdn-icons-png.flaticon.com/512/2784/2784461.png"
-}
+// var students = {
+//     id: 101,        // number
+//     sname: "Kapil", // string
+//     isPassed: true, // boolean
+//     img:"https://cdn-icons-png.flaticon.com/512/2784/2784461.png"
+// }
 
 // console.log(students);
 
-let show = `
-<div class="section">
-    <img src=${students.img} height="100px" width="100px">
-    <h1 style="background:Tomato; color:white;">Student Name: ${students.sname}</h1><hr>
-    <h2>ID: ${students.id}</h2>
-    <h2>Passed: ${students.isPassed ? "Pass out": "Pursuing"}
-</div>
-`
-document.write(show)
+// let show = `
+// <div class="section">
+//     <img src=${students.img} height="100px" width="100px">
+//     <h1 style="background:Tomato; color:white;">Student Name: ${students.sname}</h1><hr>
+//     <h2>ID: ${students.id}</h2>
+//     <h2>Passed: ${students.isPassed ? "Pass out": "Pursuing"}
+// </div>
+// `
+// document.write(show)
+
+// ------------------
+// Destructure an object
+// var employee = {
+//     id: 101,
+//     name: "Nikhil",
+//     age: 21,
+//     isEmployee: false,
+// }
+// var {id:eid, name:ename, age:eage, isEmployee:eise} = employee
+
+// document.writeln(eid)
+// document.writeln(ename)
+// document.writeln(eage)
+// document.writeln(eise)
+
+// --------------------------------
+// Update an Object
+// let student = {
+//     id: 101,
+//     name: "Devesh"
+// }
+// console.log(student["id"]);
+// console.log(student.name);
+
+// student.id = 105
+// student["name"] = "Krish"
+// student.age = 23
+
+// console.log(student["id"]);
+// console.log(student.name);
+// console.log(student.age)
+
+//------------------------
+// DElete key
+// let student = {
+//     id: 101,
+//     name: "Devesh"
+// }
+// console.log(student)
+
+// delete student.id
+
+// console.log(student)
+//-------------------------------
+// Nested object
+
+let employee = {
+    eid: 101,       // number
+    ename: "Rajeev",    // string
+    isEmployee: true,        // boolean
+    exp: {          // object
+        oracle: 2,
+        google: 3,
+        microsoft: 2,
+        deloit: 1
+    }
+}
+console.log(employee.eid)
+console.log(employee.exp.google)
+
+
+
+
+
