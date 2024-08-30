d7a334ffdbe93b9b4f13202f27b004f067d397d9
harish0916 <harish0916ojhacs@gmail.com>
2024-08-30 14:00:52 +0530

    loop2

--- PROJ7/script.js
+++ PROJ7/script.js
@@ -23,8 +23,98 @@
 //     i+=2
 // }
 
-let i=1
-do{
-    console.log(i);
-    i+=2
-}while(i<=0)
+// let i=1
+// do{
+//     console.log(i);
+//     i+=2
+// }while(i<=0)
+
+// -------------------------
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=4; j++){
+//         document.write("*");        
+//     }
+//     document.write("<br>");
+// }
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=4; j++){
+//         document.write(j);        
+//     }
+//     document.write("<br>");
+// }
+
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=i; j++){
+//         document.write("*");        
+//     }
+//     document.write("<br>");
+// }
+
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=6-i; j++){
+//         document.write("*");        
+//     }
+//     document.write("<br>");
+// }
+
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=6-i; j++){
+//         document.write("&nbsp;&nbsp;");        
+//     }
+//     for(let j=1; j<=i; j++){
+//         document.write("*");        
+//     }
+//     document.write("<br>");
+// }
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=6-i; j++){
+//         document.write("&nbsp;&nbsp;");        
+//     }
+//     for(let j=1; j<=2*i-1; j++){
+//         document.write("*");        
+//     }
+//     document.write("<br>");
+// }
+
+
+// for(let i=1; i<=5; i++){
+//     for(let j=1; j<=i; j++){
+//         document.write("&nbsp;&nbsp;");        
+//     }
+//     for(let j=1; j<=2*(6-i)-1; j++){
+//         document.write("*");        
+//     }
+//     document.write("<br>");
+// }
+
+
+// for(let i=1; i<=5; i++){
+//         for(let j=1; j<=6-i; j++){
+//             document.write("&nbsp;&nbsp;");        
+//         }
+//         for(let j=1; j<=2*i-1; j++){
+//             document.write("*");        
+//         }
+//         document.write("<br>");
+//     }
+    
+    
+//     for(let i=1; i<=5; i++){
+//         for(let j=1; j<=i; j++){
+//             document.write("&nbsp;&nbsp;");        
+//         }
+//         for(let j=1; j<=2*(6-i)-1; j++){
+//             document.write("*");        
+//         }
+//         document.write("<br>");
+//     }
+    
+
+// ---------------
+
---
+++ PROJ8/script.js
@@ -0,0 +1,42 @@
+// window.alert("Show Document")
+// let ip = window.prompt("Enter Name")
+// document.getElementById("demo").innerHTML = `<h1>Hello, ${ip}</h1>`
+// document.write("Thankyou")
+
+
+// function show(ip){
+//     // document.write("Welcome")
+//     document.getElementById("demo").innerHTML = `<h1>Welcome ${ip}</h1>`
+//     // document.getElementById("demo").innerText = `<h1>Welcome ${ip}</h1>`
+// }
+
+
+
+//---------------- INPUT
+
+// if(window.confirm("Do you want enter name"))
+//     window.prompt("Enter name")
+// else
+//     document.write("Sorry")
+
+// --------------
+// for-in loop
+// let obj = {
+//     rol: 101,
+//     name: "Ravi",
+//     age: 23
+// }
+
+// for(let k in obj){
+//     console.log(`${k}= ${obj[k]}`);
+// }
+
+// -----------------
+
+// for-of loop
+
+let arr = ['apple','banana','cherry', 'pineapple']
+
+for(let item of arr){
+    console.log(item);
+}
