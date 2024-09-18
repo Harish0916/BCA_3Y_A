8c6c372f76ee574318df6755d704ed5e7ff3f1d3
harish0916 <harish0916ojhacs@gmail.com>
2024-09-18 13:06:43 +0530

    dom update

---
+++ PROJ10/script6.js
@@ -0,0 +1,15 @@
+// let x=document.querySelector('h1')
+// x.innerHTML="Welcome"
+
+// let x1=document.querySelector('#did')
+// x1.innerHTML="Welcome did"
+
+// let x2=document.querySelector('.dclass')
+// x2.innerHTML="Welcome dclass"
+
+// let x3=document.querySelector('input[type="text"]')
+// console.log(x3.value);
+
+// let x4=document.querySelector('div')
+// x4.children[1].innerHTML="change para";
+// console.log(x4.children[1].innerHTML);
